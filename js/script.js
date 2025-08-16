3bd0ceb81993243dd36137a9ef484a07cb6fa820
patpatt9 <sitifatiharh@google.com>
2025-08-16 08:15:01 +0700

    Add header, main sections, and footer to index.html; implement welcomeSpeech and sendMessage functions in script.js; define main-logo style in style.css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// welcomeSpeech();
+
+// Call the welcomeSpeech function when the page loads
+function welcomeSpeech() {
+    let userName = prompt("Please enter your name:");
+    if (userName != '') {
+        document.getElementById('user-greeting').textContent = userName;
+    }
+}
+
+function sendMessage() {
+    let message = document.getElementById('message').value;
+    if (message !== '') {
+        alert(`Your message: "${message}" has been sent successfully!`);
+    } else {
+        alert("Please enter a message before sending.");
+    }
+}
